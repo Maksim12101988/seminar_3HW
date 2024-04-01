055237d07899294b40dc6b740ff7e79ac734e921
Maksim12101988 <124766637+Maksim12101988@users.noreply.github.com>
2024-04-01 14:48:23 +0300

    Add files via upload

---
+++ Seminar_3HW/test_1.py
@@ -0,0 +1,16 @@
+# test_1.py
+
+import logging
+from conftest import testdata
+from testpage import OperationHelper
+
+def test_step1(browsser):
+    logging.info("Test 1 Starting")
+    testpage = OperationHelper(browsser)
+    testpage.go_to_site()
+    testpage.enter_login(testdata['login'])
+    testpage.enter_pass(testdata['passwd'])
+    testpage.click_login_button()
+    assert testpage.check_login_success(), "Login failed"
+    testpage.create_post(testdata['title'], testdata['description'], testdata['content'])
+    testpage.test_contact_us() # Вызов нового теста
